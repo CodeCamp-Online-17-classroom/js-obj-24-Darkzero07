6f8bac39db5c1597414b71f7358f4eb362a9c3d5
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-26 17:40:50 +0700

    js lab 70

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// write code here
+class Sale {
+  constructor(name) {
+    this.name = name;
+    this.name = name;
+    this.name = name;
+    this.name = name;
+  }
+  calcPrice(price, discount) {
+    return price * (1 - discount);
+  }
+}
+class Beverage extends Sale {
+  constructor(name, amount, price, discount) {
+    super(name);
+    this.amount = amount;
+    this.price = price;
+    this.discount = discount;
+  }
+}
+
+const beverage = new Beverage("Pepsi", 3, 19, 0.1);
+const discountedPrice = beverage.calcPrice(beverage.price, beverage.discount);
+
+console.log(discountedPrice);
